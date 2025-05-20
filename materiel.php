677327b820612b24630925f9aafb1a13d8b9208b
Chriss17032006 <isaacsedas1@gmail.com>
2025-05-20 17:00:48 +0200

    Add files via upload

---
+++ materiel.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Matériel disponible | Emprunt</title>
+    <link rel="icon" href="../images/favicon.ico" type="image/x-icon">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <!-- HEADER -->
+    <div class="header">
+        <img src="../images/logo_universite.png" alt="Logo Université">
+    </div>
+
+    <!-- MAIN -->
+    <div class="main-container">
+        <div class="container">
+            <div class="login-container">
+
+                <!-- FIL D'ARIANE -->
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb bg-light px-3 py-2 rounded">
+                        <li class="breadcrumb-item"><a href="accueil.php">Accueil</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Matériel</li>
+                    </ol>
+                </nav>
+
+                <h2>Matériel disponible</h2>
+
+                <div class="row mt-4">
+                    <!-- Matériel 1 -->
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <img src="../images/casque_vr.jpg" class="card-img-top" alt="Casque VR">
+                            <div class="card-body">
+                                <h5 class="card-title">Casque VR</h5>
+                                <p class="card-text">Visualisation immersive pour projets 3D et simulations.</p>
+                                <a href="reserver.html" class="btn btn-primary">Réserver</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Matériel 2 -->
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <img src="../images/webcam.jpg" class="card-img-top" alt="Webcam">
+                            <div class="card-body">
+                                <h5 class="card-title">Webcam</h5>
+                                <p class="card-text">Pour faire ses meilleurs réunions.</p>
+                                <a href="reserver.html" class="btn btn-primary">Réserver</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Matériel 3 -->
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <img src="../images/drone.jpg" class="card-img-top" alt="Drone">
+                            <div class="card-body">
+                                <h5 class="card-title">Drone</h5>
+                                <p class="card-text">Pour filmer votre environnement depuis le ciel.</p>
+                                <a href="reserver.html" class="btn btn-primary">Réserver</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Matériel 4 -->
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <img src="../images/microphone.jpg" class="card-img-top" alt="Microphone">
+                            <div class="card-body">
+                                <h5 class="card-title">Microphone</h5>
+                                <p class="card-text">Enregistrement audio de qualité pour podcasts ou vidéos.</p>
+                                <a href="reserver.html" class="btn btn-primary">Réserver</a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <!-- Matériel 5 -->
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <img src="../images/cable.JPG" class="card-img-top" alt="Cable">
+                            <div class="card-body">
+                                <h5 class="card-title">Câble</h5>
+                                <p class="card-text">Câble pour faire du streaming.</p>
+                                <a href="reserver.html" class="btn btn-primary">Réserver</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Matériel 6 -->
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <img src="../images/casque et manettes vr.jpg" class="card-img-top" alt="Casque et manettes vr ">
+                            <div class="card-body">
+                                <h5 class="card-title">Casque et Manettes VR</h5>
+                                <p class="card-text">Casque et Manettes VR pour jouer ou travailler.</p>
+                                <a href="reserver.html" class="btn btn-primary">Réserver</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Matériel 7 -->
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <img src="../images/casque micro.jpg" class="card-img-top" alt="Casque micro">
+                            <div class="card-body">
+                                <h5 class="card-title">Casque micro</h5>
+                                <p class="card-text">Casque micro, pour écouter le son et parler.</p>
+                                <a href="reserver.html" class="btn btn-primary">Réserver</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Matériel 8 -->
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <img src="../images/go pro.jpg" class="card-img-top" alt="go pro">
+                            <div class="card-body">
+                                <h5 class="card-title">GoPro</h5>
+                                <p class="card-text">Pour se filmer.</p>
+                                <a href="reserver.html" class="btn btn-primary">Réserver</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Matériel 9 -->
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <img src="../images/trépied haut.jpg" class="card-img-top" alt="trépied haut ">
+                            <div class="card-body">
+                                <h5 class="card-title">Trépied haut</h5>
+                                <p class="card-text">Un morceau d'équipement pour le trépied.</p>
+                                <a href="reserver.html" class="btn btn-primary">Réserver</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Matériel 10 -->
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <img src="../images/trépied bas.jpg" class="card-img-top" alt="trépied bas">
+                            <div class="card-body">
+                                <h5 class="card-title">Trépied bas</h5>
+                                <p class="card-text">Un morceau de d'équipement pour le trépied.</p>
+                                <a href="reserver.html" class="btn btn-primary">Réserver</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Matériel 11 -->
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <img src="../images/vidéo projecteur.jpg" class="card-img-top" alt="vidéo projecteur">
+                            <div class="card-body">
+                                <h5 class="card-title">Vidéo projecteur</h5>
+                                <p class="card-text">Vidéo  projecteur qui permet de projeter des images et des sons.</p>
+                                <a href="reserver.html" class="btn btn-primary">Réserver</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Matériel 12 -->
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <img src="../images/caméra 360°.jpg" class="card-img-top" alt="caméra 360° ">
+                            <div class="card-body">
+                                <h5 class="card-title">Caméra 360°</h5>
+                                <p class="card-text">Caméra 360° qui permet de filmer tout autour d'elle même.</p>
+                                <a href="reserver.html" class="btn btn-primary">Réserver</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- FOOTER -->
+    <div class="footer">
+        <div class="footer-section">
+            <h4>Qui sommes-nous ?</h4>
+            <a href="https://www.univ-gustave-eiffel.fr/">Université Gustave Eiffel</a>
+            <a href="https://www.univ-gustave-eiffel.fr/formation/des-pedagogies-innovantes/">Centre d'Innovation Pédagogique et Numérique (CIPEN)</a>
+        </div>
+        <div class="footer-section">
+            <h4>Support</h4>
+            <a href="http://www.u-pem.fr/campus-numerique-ip/assistance/?tx_ttnews%5Bcat%5D=99&cHash=c936e533f67cbaaddda01752716910d3">FAQs</a>
+            <a href="http://www.u-pem.fr/universite/mentions-legales/">Privacy</a>
+        </div>
+        <div class="footer-section">
+            <h4>Restons en contact</h4>
+            <p>Vous pouvez nous contacter au 01 60 95 72 54, du lundi au vendredi de 9h à 17h ou par courriel</p>
+            <p>cipen@univ-eiffel.fr</p>
+        </div>
+        <div class="footer-section">
+            <h4>Suivez-nous</h4>
+            <div class="social-media">
+                <a href="https://www.facebook.com/UniversiteGustaveEiffel/"><img src="../images/facebook.png" alt="Facebook"></a>
+                <a href="https://twitter.com/UGustaveEiffel"><img src="../images/twitter.png" alt="Twitter"></a>
+                <a href="https://www.linkedin.com/company/universit%C3%A9-gustave-eiffel/"><img src="../images/linkedin.png" alt="LinkedIn"></a>
+                <a href="https://www.instagram.com/universitegustaveeiffel/"><img src="../images/instagram.png" alt="Instagram"></a>
+                <a href="https://www.youtube.com/channel/UCNMF04xs6lEAeFZ8TO6s2dw"><img src="../images/youtube.png" alt="YouTube"></a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
